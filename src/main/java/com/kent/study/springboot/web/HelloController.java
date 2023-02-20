812e22bed96b165cc4d605a24ad3c326fb916a7f
wormkent <wormkent611@gmail.com>
2023-02-21 00:36:16 +0900

    feat : HelloController에 HelloResponseDto 추가
    @GetMapping으로 HelloResponseDto 생성

--- src/main/java/com/kent/study/springboot/web/HelloController.java
+++ src/main/java/com/kent/study/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.kent.study.springboot.web;
 
+import com.kent.study.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //JSON을 반환하는 컨트롤러로 만들어준다.
@@ -10,4 +12,10 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
 }
