7fd6037ba006f37ed86366578399baf913a65498
wormkent <wormkent611@gmail.com>
2023-02-20 23:36:26 +0900

    feat : HelloController 생성
    @RestController를 붙여 JSON을 반환하는 컨트롤러 생성

---
+++ src/main/java/com/kent/study/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.kent.study.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //JSON을 반환하는 컨트롤러로 만들어준다.
+public class HelloController {
+
+    @GetMapping("/hello") //HTTP인 Get의 요청을 받을 수 있는 API를 만들어준다.
+    public String hello(){
+        return "hello";
+    }
+}
