42f7408942b510019bfb33c627c3c889b1b19616
wormkent <wormkent611@gmail.com>
2023-02-21 01:59:49 +0900

    feat : HelloResponseDto 테스트 코드 생성

--- src/main/java/com/kent/study/springboot/web/HelloController.java
+++ src/main/java/com/kent/study/springboot/web/HelloController.java
@@ -17,5 +17,7 @@ public class HelloController {
     public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name, amount);
     }
-
 }
+
+//@RequestParam("name")
+//외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
--- src/test/java/com/kent/study/springboot/web/HelloControllerTest.java
+++ src/test/java/com/kent/study/springboot/web/HelloControllerTest.java
@@ -7,8 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -28,4 +30,27 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) //mvc.perform의 결과 검증, HTTP Header의 status를 검증
                 .andExpect(content().string(hello)); //mvc.perform의 결과 검증, Controller에서 "hello"를 반환하기 때문에 이 값이 맞는지 검증
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.name", is(name)))
+                        .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
+
+//param
+//API테스트할 때 사용될 요청 파라미터를 설정
+//값은 String만 허용
+
+//jsonPath
+//JSON 응답값을 필드별로 검증할 수 있는 메서드
+//$기준으로 필드명을 명시
+//여기서는 name을 $.name으로 amount는 $.amount로 검증
