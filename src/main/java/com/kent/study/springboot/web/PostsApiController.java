e99116d392e1cfdfc122beb2de71c77d565e59e8
wormkent <wormkent611@gmail.com>
2023-02-23 23:14:39 +0900

    feat : PostsApiController 기능 추가
    update, findById 메서드 추가

--- src/main/java/com/kent/study/springboot/web/PostsApiController.java
+++ src/main/java/com/kent/study/springboot/web/PostsApiController.java
@@ -11,10 +11,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PostsApiController {
 
-    private final PostsService postService;
+    private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
-        return postService.save(requestDto);
+        return postsService.save(requestDto);
+    }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
