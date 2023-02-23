30a1a870d7c0d2176f595bdd7d3c6f82faecc354
wormkent <wormkent611@gmail.com>
2023-02-23 22:37:10 +0900

    feat : Posts 클래스 생성
    도메인 패키지에 속하는 클래스

---
+++ src/main/java/com/kent/study/springboot/domain/posts/Posts.java
@@ -0,0 +1,40 @@
+package com.kent.study.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+//주요 어노테이션을 클래스와 가깝게 둔다.
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    //JPA를 사용하면 DB 데이터에 쿼리를 날리기보다 Entity 클래스 수정을 통해 작업한다.
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
