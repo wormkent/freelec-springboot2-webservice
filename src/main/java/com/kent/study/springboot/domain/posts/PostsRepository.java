2bfd91dfd0d0a65dbfd5f1824e897f1fb08538ed
wormkent <wormkent611@gmail.com>
2023-02-23 22:39:32 +0900

    feat : PostsRepository 생성
    Posts 클래스로 db를 접근하게 해줄 PostsRepository

---
+++ src/main/java/com/kent/study/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.kent.study.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
