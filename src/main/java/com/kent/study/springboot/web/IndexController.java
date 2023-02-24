c55bd60f59058f3ad1091c67368eb12e49074018
wormkent <wormkent611@gmail.com>
2023-02-24 14:37:36 +0900

    feat: 컨트롤러에 index.mustache URL 매핑, 매핑한 URL 호출 테스트

---
+++ src/main/java/com/kent/study/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.kent.study.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index"; //src/main/resources/templates/index.mustache -> 뷰리졸버가 처리
+    }
+}
+
+//mustache 스타터로 컨트롤러에서 문자열 반환시 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
