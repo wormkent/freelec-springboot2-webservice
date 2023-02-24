c94ac7954f55fd1907d4b070a042d8c16f92e097
wormkent <wormkent611@gmail.com>
2023-02-24 16:09:56 +0900

    feat: 게시글 등록 페이지, index 컨트롤러에 save 메서드 생성

--- src/main/java/com/kent/study/springboot/web/IndexController.java
+++ src/main/java/com/kent/study/springboot/web/IndexController.java
@@ -10,6 +10,11 @@ public class IndexController {
     public String index(){
         return "index"; //src/main/resources/templates/index.mustache -> 뷰리졸버가 처리
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
 
 //mustache 스타터로 컨트롤러에서 문자열 반환시 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
